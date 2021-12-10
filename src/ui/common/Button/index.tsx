70ed9f6f65d34c5b91759fbb999ce6fcb31aa5bf
Siddhant Gupta <siddhant.c.gupta@accenture.com>
2021-12-10 10:58:17 +0200

    Add clsx package - Add example for Button component - Fixes #11

--- src/ui/common/Button/index.tsx
+++ src/ui/common/Button/index.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx'
 import React from 'react'
 
 interface ButtonProps {
@@ -26,29 +27,26 @@ export const Button = ({
   size = 'medium',
   label,
   icon = null,
-  className = '',
+  className,
   ...props
 }: ButtonProps) => {
-  const primaryClass = primary
-    ? 'text-white bg-indigo-600 hover:bg-indigo-700 border-transparent'
-    : 'text-gray-700 bg-white hover:bg-gray-50 border-gray-300'
-
-  const sizeClass =
-    size === 'small'
-      ? 'py-1 px-2 text-xs'
-      : size === 'large'
-      ? 'px-6 py-3 text-lg'
-      : 'px-4 py-2 text-sm'
-
   return (
     <button
       type="button"
-      className={`
-        ${primaryClass} 
-        ${sizeClass} 
-        font-medium inline-flex items-center  border  rounded-md shadow-sm  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
-        ${className}
-        `}
+      className={clsx(
+        primary
+          ? 'text-white bg-indigo-600 hover:bg-indigo-700 border-transparent'
+          : 'text-gray-700 bg-white hover:bg-gray-50 border-gray-300',
+        size === 'medium' && 'px-4 py-2 text-sm',
+        size === 'small' && 'py-1 px-2 text-xs',
+        size === 'large' && 'px-6 py-3 text-lg',
+        'font-medium',
+        'inline-flex items-center',
+        'border rounded-md',
+        'shadow-sm',
+        'focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500',
+        className,
+      )}
       {...props}
     >
       {icon}
