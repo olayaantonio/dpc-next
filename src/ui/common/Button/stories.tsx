085f48213239ebc81fd5e83b81e990bae2b9a97d
Siddhant Gupta <siddhant.c.gupta@accenture.com>
2021-12-10 12:02:46 +0200

    fix: Button stories

--- src/ui/common/Button/stories.tsx
+++ src/ui/common/Button/stories.tsx
@@ -21,28 +21,32 @@ export const Primary = Template.bind({})
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   primary: true,
-  label: 'Button',
+  children: 'Button',
 }
 
 export const Secondary = Template.bind({})
 Secondary.args = {
-  label: 'Button',
+  children: 'Button',
 }
 
 export const Large = Template.bind({})
 Large.args = {
   size: 'large',
-  label: 'Button',
+  children: 'Button',
 }
 
 export const Small = Template.bind({})
 Small.args = {
   size: 'small',
-  label: 'Button',
+  children: 'Button',
 }
 
 export const WithIcon = Template.bind({})
 WithIcon.args = {
-  icon: <ShoppingCartIcon className="-ml-1 mr-2 h-5 w-5" />,
-  label: 'Add to cart',
+  children: (
+    <>
+      <ShoppingCartIcon className="-ml-1 mr-2 h-5 w-5" />
+      Add to cart
+    </>
+  ),
 }
